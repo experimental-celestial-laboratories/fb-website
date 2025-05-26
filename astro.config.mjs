21be642c11001cf1376db28fe7b165c080300710
Hri7566 <hri7566@gmail.com>
2025-05-25 21:58:08 -0400

    finish new layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,9 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
